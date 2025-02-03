9451b8c9b0b485dc2f3ebf7a636311e782a54573
Aphfiwiwi <jepchirsergon@gmail.com>
2025-02-03 13:11:58 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+
+#Print is a method used to display or output a value
+print("Sergon")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,17 @@
+#Variable is a container used to store a value
+number = 20
+_number = 20
+firstname = "Sergon"
+lastname = "Jepchirchir"
+a = 10
+b = 30
+c = 50
+
+print(number)
+print(firstname)
+print(firstname,lastname)
+language = "python"
+print(language,"is a web programming langauge")
+print(a+b)
+# display the sum of 10 and 30 is 40
+print("The sum of",a,"and",b,"is",a+b)
